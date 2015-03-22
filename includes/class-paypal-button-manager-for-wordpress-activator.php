29fe0a25610120063272c3078a18149a1b8d0cec
plugingexperts <plugingexperts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-22 05:55:49 +0000

    init commit
    git-svn-id: https://plugins.svn.wordpress.org/paypal-button-manager/tags/1.0.0@1117817 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-paypal-button-manager-for-wordpress-activator.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @class       MBJ_PayPal_Button_Manager_For_WordPress_Activator
+ * @version	1.0.0
+ * @package	paypal-button-manager-for-wordpress
+ * @category	Class
+ * @author      plugingexperts <plugingexperts@gmail.com>
+ */
+class MBJ_PayPal_Button_Manager_For_WordPress_Activator {
+
+    /**
+     *
+     * @since    1.0.0
+     */
+    public static function activate() {
+        
+    }
+
+}
